f95f1c9bdf06fec60798054b0690bae7ec98d859
Guilhermessena <guilhermedvsena@gmail.com>
2023-09-13 20:41:19 -0300

    Encapsulamento (get e set)

--- bin/meu_app_oo.dart
+++ bin/meu_app_oo.dart
@@ -2,5 +2,7 @@ import 'package:meu_app_oo/classes/pessoa.dart';
 
 void main(List<String> args) {
   var p1 = Pessoa();
-  print(p1);
+  p1.setNome("Guilherme");
+  p1.setEndereco("Rua das palmas");
+  print(p1.getNome());
 }
